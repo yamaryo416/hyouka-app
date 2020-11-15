0f226d3e03060c17f7455e1519fcb2b8577d22ef
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2020-11-15 19:39:05 +0900

    sias_scales_controller_spec作成

--- app/controllers/sias_scales_controller.rb
+++ app/controllers/sias_scales_controller.rb
@@ -1,2 +1,66 @@
 class SiasScalesController < ApplicationController
+  before_action :authenticate_therapist!
+  before_action :correct_therapist?
+  before_action :set_patient
+  before_action :already_exist?
+
+  def new
+    @sias_scale = SiasScale.new
+  end
+
+  def create
+    @sias_scale = @patient.build_sias_scale(create_sias_scale_params)
+    @sias_scale.patient_id = params[:patient_id]
+    @sias_scale.save
+    flash[:success] = "患者番号:#{@patient.unique_id}のSIASを登録しました。"
+    redirect_to patients_path
+  end
+
+  private
+
+  def create_sias_scale_params
+    params.require(:sias_scale).permit(:shoulder_motor_function,
+                                       :finger_motor_function,
+                                       :hip_motor_function,
+                                       :knee_motor_function,
+                                       :foot_motor_function,
+                                       :upper_limb_muscle_tone,
+                                       :lower_limb_muscle_tone,
+                                       :upper_limb_tendon_reflex,
+                                       :lower_limb_tendon_reflex,
+                                       :upper_limb_tactile,
+                                       :lower_limb_tactile,
+                                       :upper_limb_sense_of_position,
+                                       :lower_limb_sense_of_position,
+                                       :shoulder_joint_rom,
+                                       :knee_joint_rom,
+                                       :pain,
+                                       :trunk_verticality,
+                                       :abdominal_mmt,
+                                       :visuospatial_cognition,
+                                       :speech,
+                                       :gripstrength,
+                                       :quadriceps_mmt)
+  end
+
+  def correct_therapist?
+    if !(current_therapist.patients.include?(Patient.find(params[:patient_id])) ||
+      current_therapist.has_role?(:admin))
+      redirect_to root_url
+    end
+  end
+
+  def set_patient
+    if current_therapist.has_role?(:admin)
+      @patient = Patient.find(params[:patient_id])
+    else
+      @patient = current_therapist.patients.find(params[:patient_id])
+    end
+  end
+
+  def already_exist?
+    if @patient.sias_scale
+      redirect_to @patient
+    end
+  end
 end
--- spec/requests/sias_scales_request_spec.rb
+++ spec/requests/sias_scales_request_spec.rb
@@ -1,5 +1,101 @@
 require 'rails_helper'
 
 RSpec.describe "SiasScales", type: :request do
+  let!(:admin) { create(:therapist, :admin) }
+  let!(:admin_patient) { create(:patient, therapist: admin) }
+  let!(:sias_scale) { create(:sias_scale) }
+  let!(:therapist) { create(:therapist) }
+  let!(:therapist_patient) { create(:patient, therapist: therapist) }
 
+  describe "#new" do
+    context "login as admin" do
+      before do
+        sign_in admin
+      end
+
+      it "show own patient new sias scale page" do
+        get new_patient_sias_scales_path admin_patient
+        expect(response).to render_template :new
+      end
+
+      it "show other patient new sias scale page" do
+        get new_patient_sias_scales_path therapist_patient
+        expect(response).to render_template :new
+      end
+    end
+
+    context "login as therapist" do
+      before do
+        sign_in therapist
+      end
+
+      it "show own patient new sias scale page" do
+        get new_patient_sias_scales_path therapist_patient
+        expect(response).to render_template :new
+      end
+
+      it "redirect to root url when show other patient new sias scale page" do
+        get new_patient_sias_scales_path  admin_patient
+        expect(response).to redirect_to root_url
+      end
+    end
+
+    context "not login" do
+      it "redirect to login path" do
+        get new_patient_sias_scales_path admin_patient
+        expect(response).to redirect_to new_therapist_session_path
+      end
+    end
+  end
+
+  describe "#ceate" do
+    let!(:sias_scale_params) { attributes_for(:sias_scale) }
+
+    context "login as admin" do
+      before do
+        sign_in admin
+      end
+
+      it "is success to create own patient sias scale" do
+        expect do
+          post patient_sias_scales_path(admin_patient), params: { sias_scale: sias_scale_params }
+        end.to change(SiasScale, :count).by 1
+      end
+
+      it "is success to create other patient sias scale" do
+        expect do
+          post patient_sias_scales_path(therapist_patient), params: { sias_scale: sias_scale_params }
+        end.to change(SiasScale, :count).by 1
+      end
+
+      it "is correct message" do
+        post patient_sias_scales_path(admin_patient), params: { sias_scale: sias_scale_params }
+        expect(flash[:success]).to eq "患者番号:#{admin_patient.unique_id}のSIASを登録しました。"
+      end
+    end
+
+    context "login as therapist" do
+      before do
+        sign_in therapist
+      end
+
+      it "is success to create own patient sias scale" do
+        expect do
+          post patient_sias_scales_path(therapist_patient), params: { sias_scale: sias_scale_params }
+        end.to change(SiasScale, :count).by 1
+      end
+
+      it "redirect to root url when create other patient sias scale" do
+        post patient_sias_scales_path(admin_patient), params: { sias_scale: sias_scale_params }
+        expect(response).to redirect_to root_url
+      end
+    end
+
+    context "not login" do
+      it "redirect to login path" do
+        post patient_sias_scales_path(admin_patient), params: { sias_scale: sias_scale_params }
+        expect(response).to redirect_to new_therapist_session_path
+      end
+    end
+  end
 end
